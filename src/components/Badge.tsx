4f1edfac502df79a28db2c13b1c84db4083873e2
rizzioli <rizzioli@oaservice.it>
2023-11-21 15:42:41 +0100

    NavbarRemovedChatFull

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -8,67 +8,67 @@ type Props = {
 
 const defaultTextColor = "#303235";
 
-export const Badge = (props: Props) => {
-  let liteBadge: HTMLAnchorElement | undefined;
-  let observer: MutationObserver | undefined;
+// export const Badge = (props: Props) => {
+//   let liteBadge: HTMLAnchorElement | undefined;
+//   let observer: MutationObserver | undefined;
 
-  const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
-    mutations.forEach((mutation) => {
-      mutation.removedNodes.forEach((removedNode) => {
-        if (
-          "id" in removedNode &&
-          liteBadge &&
-          removedNode.id == "lite-badge"
-        ) {
-          console.log("Sorry, you can't remove the brand 😅");
-          props.botContainer?.append(liteBadge);
-        }
-      });
-    });
-  };
+//   const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
+//     mutations.forEach((mutation) => {
+//       mutation.removedNodes.forEach((removedNode) => {
+//         if (
+//           "id" in removedNode &&
+//           liteBadge &&
+//           removedNode.id == "lite-badge"
+//         ) {
+//           console.log("Sorry, you can't remove the brand 😅");
+//           props.botContainer?.append(liteBadge);
+//         }
+//       });
+//     });
+//   };
 
-  onMount(() => {
-    if (!document || !props.botContainer) return;
-    observer = new MutationObserver(appendBadgeIfNecessary);
-    observer.observe(props.botContainer, {
-      subtree: false,
-      childList: true,
-    });
-  });
+//   onMount(() => {
+//     if (!document || !props.botContainer) return;
+//     observer = new MutationObserver(appendBadgeIfNecessary);
+//     observer.observe(props.botContainer, {
+//       subtree: false,
+//       childList: true,
+//     });
+//   });
 
-  onCleanup(() => {
-    if (observer) observer.disconnect();
-  });
+//   onCleanup(() => {
+//     if (observer) observer.disconnect();
+//   });
 
-  return (
-    <span
-      style={{
-        "font-size": "13px",
-        position: "absolute",
-        bottom: 0,
-        padding: "10px",
-        margin: "auto",
-        width: "100%",
-        "text-align": "center",
-        color: props.poweredByTextColor ?? defaultTextColor,
-        "background-color": props.badgeBackgroundColor ?? "#ffffff",
-      }}
-    >
-      Powered by
-      <a
-        ref={liteBadge}
-        href={"https://leonvanzyl.com"}
-        target="_blank"
-        rel="noopener noreferrer"
-        class="lite-badge"
-        id="lite-badge"
-        style={{
-          "font-weight": "bold",
-          color: props.poweredByTextColor ?? defaultTextColor,
-        }}
-      >
-        <span> Leon van Zyl</span>
-      </a>
-    </span>
-  );
-};
+//   return (
+//     <span
+//       style={{
+//         "font-size": "13px",
+//         position: "absolute",
+//         bottom: 0,
+//         padding: "10px",
+//         margin: "auto",
+//         width: "100%",
+//         "text-align": "center",
+//         color: props.poweredByTextColor ?? defaultTextColor,
+//         "background-color": props.badgeBackgroundColor ?? "#ffffff",
+//       }}
+//     >
+//       Powered by
+//       <a
+//         ref={liteBadge}
+//         href={"https://leonvanzyl.com"}
+//         target="_blank"
+//         rel="noopener noreferrer"
+//         class="lite-badge"
+//         id="lite-badge"
+//         style={{
+//           "font-weight": "bold",
+//           color: props.poweredByTextColor ?? defaultTextColor,
+//         }}
+//       >
+//         <span> Leon van Zyl</span>
+//       </a>
+//     </span>
+//   );
+// };
